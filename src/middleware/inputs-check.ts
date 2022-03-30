64c624a57f1f87a001ab2a0c0ac28ed14d3c61c1
TheNewDevl <95215674+TheNewDevl@users.noreply.github.com>
2022-03-30 19:59:40 +0200

    inputs validation

--- src/middleware/inputs-check.ts
+++ src/middleware/inputs-check.ts
@@ -1,39 +1,39 @@
 import { NextFunction, Request, Response } from 'express'
 
+// Temporary validators, in a future version a module will be used to validate all data
 
 const heatChecker = (heat: number): boolean => {
     return typeof heat === 'number' && heat >= 0 && heat <= 10
+}
+const nameChecker = (name: string): boolean => {
+    return /^([a-zA-Zéàè@êûâç'"-][\s]{0,10}){3,30}$/gm.test(name) && typeof name === 'string'
+}
+const manufacturerChecker = (manufacturer: string): boolean => {
+    return /^([a-zA-Zéàè@êûâç'"-][\s]{0,10}){3,30}$/gm.test(manufacturer) && typeof manufacturer === 'string'
+}
+const descriptionChecker = (description: string): boolean => {
+    return /^([a-zA-Zéàè@êûâç'"-][\s]{0,10}){3,30}$/gm.test(description) && typeof description === 'string'
+}
+const mainPepperChecker = (mainPepper: string): boolean => {
+    return /^([a-zA-Zéàè@êûâç'"-][\s]{0,10}){3,30}$/gm.test(mainPepper) && typeof mainPepper === 'string'
 }
 
 export default (req: Request, res: Response, next: NextFunction) => {
     try {
         if (!heatChecker(req.body.heat)) {
-            throw 'Heat must be a number between 0 and 10'
+            throw 'Doit être un chiffre entre 0 et 10'
+        } else if (!nameChecker(req.body.name)) {
+            throw 'Des caractères interdits ont été utilisés'
+        } else if (!manufacturerChecker(req.body.manufacturer)) {
+            throw 'Des caractères interdits ont été utilisés'
+        } else if (!descriptionChecker(req.body.description)) {
+            throw 'Des caractères interdits ont été utilisés'
+        } else if (!mainPepperChecker(req.body.mainPepper)) {
+            throw 'Des caractères interdits ont été utilisés'
+        } else {
+            next()
         }
-
-
     } catch (error) {
         res.status(400).json({ error })
     }
-
-
 }
-
-
-
-/* 
-{
-    "_id": "624440cc700962723dc726c7",
-        "userId": "6242f314389dcf06a0b53ea3",
-            "name": "azazazaz",
-                "manufacturer": "zqetzeq",
-                    "description": "tqzetzqet",
-                        "mainPepper": "qzetq",
-                            "imageUrl": "http://localhost:3000/images/Capture_d’écran_2022-02-11_à_10.47.04.png1648640204354.png",
-                                "heat": 2,
-                                    "likes": 0,
-                                        "dislikes": 0,
-                                            "usersLiked": [],
-                                                "usersDisliked": [],
-                                                    "__v": 0
-} */
